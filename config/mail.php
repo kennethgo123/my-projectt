423d5a0fa1f2a4b45246cb9c806aaf3f4aa8ca8d
Admin <admin@Admins-MacBook-Pro.local>
2025-10-19 05:06:52 +0800

    Add SendGrid mailer configuration to fix registration email error

--- config/mail.php
+++ config/mail.php
@@ -49,6 +49,16 @@ return [
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
+        'sendgrid' => [
+            'transport' => 'smtp',
+            'host' => env('SENDGRID_HOST', 'smtp.sendgrid.net'),
+            'port' => env('SENDGRID_PORT', 587),
+            'encryption' => env('SENDGRID_ENCRYPTION', 'tls'),
+            'username' => env('SENDGRID_USERNAME', 'apikey'),
+            'password' => env('SENDGRID_API_KEY'),
+            'timeout' => null,
+        ],
+
         'ses' => [
             'transport' => 'ses',
         ],
