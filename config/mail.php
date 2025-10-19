3ee539f1c63ffc661c72e66efc7c3f018ef78c36
Admin <admin@Admins-MacBook-Pro.local>
2025-10-19 22:29:47 +0800

    Add SendGrid email driver permanently

--- config/mail.php
+++ config/mail.php
@@ -50,13 +50,7 @@ return [
         ],
 
         'sendgrid' => [
-            'transport' => 'smtp',
-            'host' => env('SENDGRID_HOST', 'smtp.sendgrid.net'),
-            'port' => env('SENDGRID_PORT', 587),
-            'encryption' => env('SENDGRID_ENCRYPTION', 'tls'),
-            'username' => env('SENDGRID_USERNAME', 'apikey'),
-            'password' => env('SENDGRID_API_KEY'),
-            'timeout' => null,
+            'transport' => 'sendgrid',
         ],
 
         'ses' => [
